9eaf6860eaab4a3eb12d4d12f240fa5b44c8b6dc
Jonathan Dey <j.dey@node4.co.uk>
2019-03-07 14:54:45 +0000

    Connected to redis, pushing data to ws

--- app/index.js
+++ app/index.js
@@ -1,5 +1,41 @@
 const io = require('socket.io')(80);
+const redis = require('redis');
+require('dotenv').config()
+
+const redisClient = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD,
+    db: 0
+});
+
+const pub = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD,
+    db: 0
+});
+
+redisClient.on('error', err => console.error('Error connecting to redis', err));
+redisClient.on('ready', () => {
+    console.log('Connected to redis')
+});
+
+const connectedSockets = [];
+
+redisClient.on('message', (channel, message) => {
+    console.log(channel, message)
+
+    if (channel === 'alerts') {
+        io.sockets.emit(channel, message)
+    }
+});
+
+redisClient.subscribe('alerts');
+
 
 io.on('connection', (socket) => {
-   console.log('user connected')
+   console.log('user connected');
+
+   connectedSockets.push(socket);
 });
