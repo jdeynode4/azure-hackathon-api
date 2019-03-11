5c762bde86b03f80aaba9457c29d2806e4462127
Jonathan Dey <j.dey@node4.co.uk>
2019-03-11 16:37:25 +0000

    Annotated code

---
+++ README.md
@@ -0,0 +1,9 @@
+# N4 Hackathon Websocket API (Team Node7)
+
+### Getting Started
+
+1. Have Docker installed
+
+2. `docker-compose up`
+
+This repo is just for the API. You will need the [front-end code](https://github.com/SmithDev1237/Hackathon-frontend) & Azure setup to get using it.
--- app/index.js
+++ app/index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const io = require('socket.io')(80);
 const redis = require('redis');
-const Push = require( 'pushover-notifications' )
+const Push = require('pushover-notifications');
+let notify = null;
 
+// Setup the connection to Redis
 const redisClient = redis.createClient({
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
@@ -10,8 +12,12 @@ const redisClient = redis.createClient({
     db: 0
 });
 
-if (process.env.PUSHOVER_ENABLED === true) {
-    const notify = new Push({
+// Setup the connection to Pushover
+// Is Pushover enabled?
+if (process.env.PUSHOVER_ENABLED === 'true') {
+    console.log('Starting pushover...');
+
+    notify = new Push({
         user: process.env.PUSHOVER_USER,
         token: process.env.PUSHOVER_TOKEN,
         onerror: (err) => {
@@ -20,22 +26,28 @@ if (process.env.PUSHOVER_ENABLED === true) {
     });
 }
 
+// Listen for either a ready state on Redis or an error
 redisClient.on('error', err => console.error('Error connecting to redis', err));
 redisClient.on('ready', () => {
     console.log('Connected to redis')
 });
 
-const connectedSockets = [];
+// Sub to the Redis alerts channel
+redisClient.subscribe('alerts');
 
+// Listen for Pub'd messages on Redis
 redisClient.on('message', (channel, message) => {
     console.log(channel, message)
 
+    // We have a message on the alerts channel, time to pass it on!
     if (channel === 'alerts') {
-        io.sockets.emit(channel, message)
+        // Pass the message to all sockets listening on the same 'alerts' channel
+        io.sockets.emit(channel, message);
 
-        const alert = JSON.parse(message)
 
-        if (alert.status == 'Error' && process.env.PUSHOVER_ENABLED === true) {
+        const alert = JSON.parse(message);
+        // If the alert is of status 'Error' (an intruder) & Pushover is enabled; send a push notification
+        if (alert.status === 'Error' && process.env.PUSHOVER_ENABLED === 'true') {
             notify.send({
                 title: `Security Alert`,
                 message: `${alert.name}: ${alert.text}`,
@@ -45,11 +57,7 @@ redisClient.on('message', (channel, message) => {
     }
 });
 
-redisClient.subscribe('alerts');
-
-
+// When a new socket connects log out a message
 io.on('connection', (socket) => {
    console.log('user connected');
-
-   connectedSockets.push(socket);
 });
